b5a16e1c7ba3a0843ac2eb6cd483ac3670aa3481
namnguyen215 <namnguyenphuong215@gmail.com>
2022-05-10 16:47:13 +0700

    a

--- runPPCV.sh
+++ runPPCV.sh
@@ -2,10 +2,7 @@ spark-submit --class ppcvTask\
     --master yarn \
     --driver-cores 2 \
     --driver-memory 2G \
-    --num-executors 10 \
+    --num-executors 5 \
     --executor-cores 2 \
     --executor-memory 2G \
-    --conf spark.dynamicAllocation.minExecutors=5 \
-    --conf spark.dynamicAllocation.maxExecutors=30 \
-    --conf spark.dynamicAllocation.initialExecutors=10\
     target/InternTask-1.0-SNAPSHOT.jar
